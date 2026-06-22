fba39f13595802863264e05c78b4eeb0e80da769
GabrielMeirellesCury <curymgabriel@gmail.com>
2026-06-22 17:08:42 -0300

    arrumei as imagens

--- backend/server.js
+++ backend/server.js
@@ -17,37 +17,37 @@ async function start() {
     {
       name: "Poção Blue Sky",
       description: "Provê um surto de inspiração por 24 horas. Utilizada por John Lennon quando escreveu Lucy in the Sky with Diamonds.",
-      image: "https://i.imgur.com/placeholder.png",
+      image: "https://i.ibb.co/ZzS7xb2/rsz-sky.png",
       price: 300
     },
     {
       name: "Poção do Perfume Misterioso",
       description: "Faz com que você fique cheirando lilás e groselha por 24 dias. Essência muito admirada pelos bruxos.",
-      image: "https://i.imgur.com/placeholder.png",
+      image: "https://i.ibb.co/pyhZJXf/rsz-lilas.png",
       price: 200
     },
     {
       name: "Poção de Pinus",
       description: "Faz com que você fique 10 cm mais alto! Observação: efeitos colaterais desconhecidos.",
-      image: "https://i.imgur.com/placeholder.png",
+      image: "https://i.ibb.co/DkzdL1q/rsz-pinus.png",
       price: 3000
     },
     {
       name: "Poção da Beleza Eterna",
       description: "Veneno que mata rápido.",
-      image: "https://i.imgur.com/placeholder.png",
+      image: "https://i.ibb.co/9p872NK/rsz-1beleza.png",
       price: 100
     },
     {
       name: "Poção do Arco Íris",
       description: "Traz felicidade momentânea. Pode durar de 10 minutos a 2 dias.",
-      image: "https://i.imgur.com/placeholder.png",
+      image: "https://i.ibb.co/PrC09MP/rsz-2unicornio.png",
       price: 120
     },
     {
       name: "Caldeirão das Verdades Secretas",
       description: "As pessoas lhe dirão apenas verdades por 1 hora. É necessário beber os 5L.",
-      image: "https://i.imgur.com/placeholder.png",
+      image: "https://i.ibb.co/s9Lyvj8/rsz-verdades.png",
       price: 150
     }
   ])
